30b0696bccea8c2653faf5b08feac16826dc28b7
Lukas <luk.brasiunas@gmail.com>
2024-02-19 22:43:27 +0200

    Remove docker-compose launch option and adjust settings for final application

--- Infrastructure/Migrations/20240218125336_InitialDatabaseTables.cs
+++ Infrastructure/Migrations/20240218125336_InitialDatabaseTables.cs
@@ -58,17 +58,62 @@ namespace Infrastructure.Migrations
                 columns: new[] { "Brand", "Model", "Year" },
                 values: new object[] { "Toyota", "Camry", 2022 });
 
+            migrationBuilder.InsertData(
+                table: "Cars",
+                columns: new[] { "Brand", "Model", "Year" },
+                values: new object[] { "Honda", "Accord", 2020 });
+
+            migrationBuilder.InsertData(
+                table: "Cars",
+                columns: new[] { "Brand", "Model", "Year" },
+                values: new object[] { "Ford", "Fusion", 2019 });
+
+            migrationBuilder.InsertData(
+                table: "Cars",
+                columns: new[] { "Brand", "Model", "Year" },
+                values: new object[] { "Chevrolet", "Malibu", 2018 });
+
+            migrationBuilder.InsertData(
+                table: "Cars",
+                columns: new[] { "Brand", "Model", "Year" },
+                values: new object[] { "Tesla", "Model S", 2023 });
+
             // Add data to Services table
             migrationBuilder.InsertData(
                 table: "Services",
                 columns: new[] { "Name", "Description" },
                 values: new object[] { "Berauto", "We fix everything related to cars" });
 
-            // Add data to CarsAssignedToServices table (assuming some ServiceId and CarId values)
+            migrationBuilder.InsertData(
+                table: "Services",
+                columns: new[] { "Name", "Description" },
+                values: new object[] { "QuickFix", "Fast and efficient car repairs" });
+
+            migrationBuilder.InsertData(
+                table: "Services",
+                columns: new[] { "Name", "Description" },
+                values: new object[] { "TuneUp", "Get your car running smoothly" });
+
+            migrationBuilder.InsertData(
+                table: "Services",
+                columns: new[] { "Name", "Description" },
+                values: new object[] { "Detailing", "Make your car look brand new" });
+
+            migrationBuilder.InsertData(
+                table: "Services",
+                columns: new[] { "Name", "Description" },
+                values: new object[] { "OilChange", null });
+
+            // Add data to CarsAssignedToServices table
             migrationBuilder.InsertData(
                 table: "CarsAssignedToServices",
                 columns: new[] { "ServiceId", "CarId" },
-                values: new object[] { 1, 1 });
+                values: new object[] { 1, 2 });
+
+            migrationBuilder.InsertData(
+                table: "CarsAssignedToServices",
+                columns: new[] { "ServiceId", "CarId" },
+                values: new object[] { 2, 4 });
         }
 
         /// <inheritdoc />
